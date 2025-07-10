1e2f9e857c0135f9b419f674e7df1841b2eea66a
Abdul Monim <43042797+Monim752@users.noreply.github.com>
2025-07-10 11:41:12 +0600

    Added setting.py

--- project_management/settings.py
+++ project_management/settings.py
@@ -1,1 +1,13 @@
-# Settings placeholder
+INSTALLED_APPS = [
+    ...
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'drf_yasg',
+    'api',
+]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
